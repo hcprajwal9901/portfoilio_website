a4444d1f3ca7d5f85d5f983d3d45aab556a2b68b
H C Prajwal <hcprajwal9901@gmail.com>
2025-12-01 09:53:44 +0530

    change base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nodejs_ci_cd_task/', // <--- IMPORTANT: Change 'your-repo-name' to your actual repo name!
+  base: '/portfoilio_website/', // <--- IMPORTANT: Change 'your-repo-name' to your actual repo name!
   // Example: If your repo is named 'my-portfolio-app', it would be '/my-portfolio-app/'
 });
